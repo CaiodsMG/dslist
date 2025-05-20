8627b5d4c91d4b72bc6621f728feb073669b333e
CaiodsMG <caiocfn10@gmail.com>
2025-05-20 14:30:41 -0300

    Get game by id, exceptions

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,11 +1,12 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +18,15 @@ public class GameController {
     private GameService gameService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<GameMinDTO> findAll(){
         List<GameMinDTO> result = gameService.findAll();
         return result;
+    }
+
+    @GetMapping("/{gameId}")
+    public ResponseEntity<GameDTO> findById(@PathVariable Long gameId){
+        GameDTO result = gameService.findById(gameId);
+        return ResponseEntity.ok(result);
     }
 }
---
+++ src/main/java/com/devsuperior/dslist/exceptions/GameNotFound.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dslist.exceptions;
+
+public class GameNotFound extends RuntimeException{
+
+    public GameNotFound (String message){
+        super(message);
+    }
+}
