f9750f8ec63bd3efd92654b3049a97828f22d9fc
CaiodsMG <caiocfn10@gmail.com>
2025-05-25 17:06:52 -0300

    novo DTO, novo endpoint de movimentação

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +16,28 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     public List<GameListDTO> findAll(){
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+    public void move(Long listId, int posicaoOrigem, int posicaoDestino){
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(posicaoOrigem);
+        list.add(posicaoDestino, obj);
+
+        int min = posicaoOrigem < posicaoDestino ? posicaoOrigem : posicaoDestino;
+        int max = posicaoOrigem < posicaoDestino ? posicaoDestino : posicaoOrigem;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+
+
+
     }
 }
